c0f9d8c672abd6796a317720ff592179791ea683
TypeDefinition <TypeDefinition@Outlook.sg>
2025-02-15 13:37:54 -0500

    Cubemaps now only render faces which are visible.

--- CavernSDK/Packages/com.spelunx.cavern.sdk/Runtime/Scripts/CavernCamera/CavernRenderer.cs
+++ CavernSDK/Packages/com.spelunx.cavern.sdk/Runtime/Scripts/CavernCamera/CavernRenderer.cs
@@ -28,11 +28,11 @@ namespace Spelunx {
         [Header("Camera Settings")]
         [SerializeField] private StereoscopicMode stereoMode = StereoscopicMode.Mono;
         [SerializeField] private CubemapResolution cubemapResolution = CubemapResolution.Mid;
-        [SerializeField, Range(0.05f, 0.08f)] private float interpupillaryDistance = 0.065f; // IPD in metres.
+        [SerializeField, Range(0.055f, 0.075f)] private float interpupillaryDistance = 0.065f; // IPD in metres.
         [SerializeField, Min(0.1f)] private float cavernHeight = 2.0f; // Cavern physical screen height in metres.
         [SerializeField, Min(0.1f)] private float cavernRadius = 3.0f; // Cavern physical screen radius in metres.
         [SerializeField, Min(0.1f)] private float cavernAngle = 270.0f; // Cavern physical screen angle in degrees.
-        [SerializeField, Range(-1.0f, 1.0f)] private float cavernElevation = 0.0f; // Cavern physical screen elevation off the floor in metres.
+        [SerializeField, Range(-0.5f, 0.5f)] private float cavernElevation = 0.0f; // Cavern physical screen elevation relative to where the player is standing.
 
         [Header("References")]
         [SerializeField] private Transform head;
@@ -104,14 +104,106 @@ namespace Spelunx {
         }
 
         private void RenderEyes() {
+            // TODO: Clean this code up. And document it so that it's not just some scribbles on paper.
+            // Find the intersection points.
+            // Solve quadratic equation for Line 1:
+            float c = -(cavernRadius * cavernRadius - head.transform.localPosition.x * head.transform.localPosition.x - head.transform.localPosition.z * head.transform.localPosition.z) * 0.5f;
+            float b1 = head.transform.localPosition.x + head.transform.localPosition.z;
+            float t1 = (-b1 + Mathf.Sqrt(b1 * b1 - 4 * c)) / 2.0f;
+            float t2 = (-b1 - Mathf.Sqrt(b1 * b1 - 4 * c)) / 2.0f;
+
+            // TODO: Deal with cases when b^2 - 4ac < 0.
+
+            // Solve quadratic equation for Line 2:
+            float b2 = head.transform.localPosition.x - head.transform.localPosition.z;
+            float s1 = (-b2 + Mathf.Sqrt(b2 * b2 - 4 * c)) / 2.0f;
+            float s2 = (-b2 - Mathf.Sqrt(b2 * b2 - 4 * c)) / 2.0f;
+
+            Vector3 A = new Vector3(Mathf.Max(t1, t2), 0.0f, Mathf.Max(t1, t2));
+            Vector3 C = new Vector3(Mathf.Min(t1, t2), 0.0f, Mathf.Min(t1, t2));
+            Vector3 B = new Vector3(Mathf.Min(s1, s2), 0.0f, -Mathf.Min(s1, s2));
+            Vector3 D = new Vector3(Mathf.Max(s1, s2), 0.0f, -Mathf.Max(s1, s2));
+
+            A += new Vector3(head.transform.localPosition.x, 0.0f, head.transform.localPosition.z);
+            B += new Vector3(head.transform.localPosition.x, 0.0f, head.transform.localPosition.z);
+            C += new Vector3(head.transform.localPosition.x, 0.0f, head.transform.localPosition.z);
+            D += new Vector3(head.transform.localPosition.x, 0.0f, head.transform.localPosition.z);
+
             const int leftMask = 1 << (int)CubemapFace.PositiveX;
             const int rightMask = 1 << (int)CubemapFace.NegativeX;
             const int topMask = 1 << (int)CubemapFace.PositiveY;
             const int bottomMask = 1 << (int)CubemapFace.NegativeY;
             const int frontMask = 1 << (int)CubemapFace.PositiveZ;
             const int backMask = 1 << (int)CubemapFace.NegativeZ;
-            // const int allMask = leftMask | rightMask | topMask | bottomMask | frontMask | backMask;
-            const int allMask = 0;
+
+            string debugStr = "Rendering: Front";
+
+            int leftRightEyeMask = frontMask;
+            if (Vector3.Angle(C, Vector3.forward) < cavernAngle * 0.5f || Vector3.Angle(D, Vector3.forward) < cavernAngle * 0.5f) {
+                leftRightEyeMask |= backMask;
+                debugStr += ", Back";
+            }
+
+            int frontBackEyeMask = 0;
+            if (Vector3.Angle(A, Vector3.forward) < cavernAngle * 0.5f || Vector3.Angle(D, Vector3.forward) < cavernAngle * 0.5f) {
+                frontBackEyeMask |= rightMask;
+                debugStr += ", Right";
+            }
+            if (Vector3.Angle(B, Vector3.forward) < cavernAngle * 0.5f || Vector3.Angle(C, Vector3.forward) < cavernAngle * 0.5f) {
+                frontBackEyeMask |= leftMask;
+                debugStr += ", Left";
+            }
+
+            A -= new Vector3(head.transform.localPosition.x, 0.0f, head.transform.localPosition.z);
+            B -= new Vector3(head.transform.localPosition.x, 0.0f, head.transform.localPosition.z);
+            C -= new Vector3(head.transform.localPosition.x, 0.0f, head.transform.localPosition.z);
+            D -= new Vector3(head.transform.localPosition.x, 0.0f, head.transform.localPosition.z);
+
+            Vector3 forwardQuadTopCheck = (A.z < B.z) ? A : B;
+            forwardQuadTopCheck.y = cavernElevation + cavernHeight - head.transform.localPosition.y;
+            Vector3 backQuadTopCheck = (C.z > D.z) ? C : D;
+            backQuadTopCheck.y = cavernElevation + cavernHeight - head.transform.localPosition.y;
+            
+            if (Mathf.Abs(forwardQuadTopCheck.z) < Mathf.Abs(forwardQuadTopCheck.y) ||
+                Mathf.Abs(backQuadTopCheck.z) < Mathf.Abs(backQuadTopCheck.y)) {
+                leftRightEyeMask |= topMask;
+                debugStr += ", Horizontal Top";
+            }
+
+            Vector3 forwardQuadBottomCheck = (A.z < B.z) ? A : B;
+            forwardQuadBottomCheck.y = cavernElevation - head.transform.localPosition.y;
+            Vector3 backQuadBottomCheck = (C.z > D.z) ? C : D;
+            backQuadBottomCheck.y = cavernElevation - head.transform.localPosition.y;
+
+            if (Mathf.Abs(forwardQuadBottomCheck.z) < Mathf.Abs(forwardQuadBottomCheck.y) ||
+                Mathf.Abs(backQuadBottomCheck.z) < Mathf.Abs(backQuadBottomCheck.y)) {
+                leftRightEyeMask |= bottomMask;
+                debugStr += ", Horizontal Bottom";
+            }
+
+            Vector3 leftQuadTopCheck = (B.x > C.x) ? B : C;
+            leftQuadTopCheck.y = cavernElevation + cavernHeight - head.transform.localPosition.y;
+            Vector3 rightQuadTopCheck = (A.x < D.x) ? A : D;
+            rightQuadTopCheck.y = cavernElevation + cavernHeight - head.transform.localPosition.y;
+            
+            if (Mathf.Abs(leftQuadTopCheck.x) < Mathf.Abs(leftQuadTopCheck.y) ||
+                Mathf.Abs(rightQuadTopCheck.x) < Mathf.Abs(rightQuadTopCheck.y)) {
+                frontBackEyeMask |= topMask;
+                debugStr += ", Vertical Top";
+            }
+
+            Vector3 leftQuadBottomCheck = (B.x > C.x) ? B : C;
+            leftQuadTopCheck.y = cavernElevation - head.transform.localPosition.y;
+            Vector3 rightQuadBottomCheck = (A.x < D.x) ? A : D;
+            rightQuadTopCheck.y = cavernElevation - head.transform.localPosition.y;
+
+            if (Mathf.Abs(leftQuadBottomCheck.x) < Mathf.Abs(leftQuadBottomCheck.y) ||
+                Mathf.Abs(rightQuadTopCheck.x) < Mathf.Abs(rightQuadTopCheck.y)) {
+                frontBackEyeMask |= bottomMask;
+                debugStr += ", Vertical Bottom";
+            }
+
+            // Debug.Log(debugStr);
 
             // Use Camera.MonoOrStereoscopicEye.Left or Camera.MonoOrStereoscopicEye.Right to ensure that the cubemap follows the camera's rotation.
             // Camera.MonoOrStereoscopicEye.Mono renders the cubemap to be aligned to the world's axes instead.
@@ -120,19 +212,19 @@ namespace Spelunx {
                     eye.stereoSeparation = 0.0f;
                     eye.transform.rotation = gameObject.transform.rotation; // Set eye's global orientation to the screen's orientation, regardless of the head's orientation.
                     eye.transform.localPosition = Vector3.zero;
-                    eye.RenderToCubemap(cubemaps[(int)CubemapIndex.Left], allMask | frontMask | leftMask | rightMask, Camera.MonoOrStereoscopicEye.Left);
+                    eye.RenderToCubemap(cubemaps[(int)CubemapIndex.Left], leftRightEyeMask | frontBackEyeMask, Camera.MonoOrStereoscopicEye.Left);
                     break;
                 case StereoscopicMode.Stereo:
                     eye.stereoSeparation = 0.0f;
                     eye.transform.rotation = gameObject.transform.rotation; // Set eye's global orientation to the screen's orientation, regardless of the head's orientation.
-                    eye.transform.localPosition = new Vector3(-interpupillaryDistance * 0.5f, 0.0f, 0.0f);
-                    eye.RenderToCubemap(cubemaps[(int)CubemapIndex.Left], allMask | frontMask, Camera.MonoOrStereoscopicEye.Left);
+                    eye.transform.localPosition = new Vector3(interpupillaryDistance * -0.5f, 0.0f, 0.0f);
+                    eye.RenderToCubemap(cubemaps[(int)CubemapIndex.Left], leftRightEyeMask, Camera.MonoOrStereoscopicEye.Left);
                     eye.transform.localPosition = new Vector3(interpupillaryDistance * 0.5f, 0.0f, 0.0f);
-                    eye.RenderToCubemap(cubemaps[(int)CubemapIndex.Right], allMask | frontMask, Camera.MonoOrStereoscopicEye.Right);
+                    eye.RenderToCubemap(cubemaps[(int)CubemapIndex.Right], leftRightEyeMask, Camera.MonoOrStereoscopicEye.Right);
                     eye.transform.localPosition = new Vector3(0.0f, 0.0f, interpupillaryDistance * 0.5f);
-                    eye.RenderToCubemap(cubemaps[(int)CubemapIndex.Front], allMask | leftMask | rightMask, Camera.MonoOrStereoscopicEye.Left);
-                    eye.transform.localPosition = new Vector3(0.0f, 0.0f, -interpupillaryDistance * 0.5f);
-                    eye.RenderToCubemap(cubemaps[(int)CubemapIndex.Back], allMask | leftMask | rightMask, Camera.MonoOrStereoscopicEye.Right);
+                    eye.RenderToCubemap(cubemaps[(int)CubemapIndex.Front], frontBackEyeMask, Camera.MonoOrStereoscopicEye.Left);
+                    eye.transform.localPosition = new Vector3(0.0f, 0.0f, interpupillaryDistance * -0.5f);
+                    eye.RenderToCubemap(cubemaps[(int)CubemapIndex.Back], frontBackEyeMask, Camera.MonoOrStereoscopicEye.Right);
                     eye.transform.localPosition = Vector3.zero;
                     break;
             }
