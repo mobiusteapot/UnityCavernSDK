ef3bd41897787221316bd978584ad1c0313974ac
Joshua Kaplan <jgkaplan@andrew.cmu.edu>
2025-03-13 17:17:24 -0400

    Will do more keyboard stuff later. Merging now for south fayette

--- CavernSDK/Packages/com.spelunx.cavern.sdk/Runtime/Scripts/Keyboard/DebugManager.cs
+++ CavernSDK/Packages/com.spelunx.cavern.sdk/Runtime/Scripts/Keyboard/DebugManager.cs
@@ -11,19 +11,32 @@ namespace Spelunx
 
         // [SerializeField]
         // private List<InputAction> inputs;
+        // private InputActionMap actions;
+
+        // InputActionMap a;
 
         [SerializeField]
         private InputAction quit;
 
-        // void RegisterInput(InputAction act)
+        // public void RegisterInput(string name, InputAction act)
         // {
-        //     inputs.Add(act);
+        //     // Don't add the action if the map already has it
+        //     if(actions.Contains(act)){
+        //         return;
+        //     }
+        //     actions.AddAction(name, act);
         // }
 
         void Awake()
         {
             quit.performed += QuitAction;
             quit.Enable();
+            // a.AddAction();
+            // a.
+            // foreach (InputAction act in inputs)
+            // {
+            //     act.Enable();
+            // }
         }
         public void QuitAction(InputAction.CallbackContext ctx)
         {
